6d674f505b6d68cafa031aeb50e56059cc02ba84
Alex Skrivseth <alexskrivseth@gmail.com>
2020-07-01 19:21:50 -0700

    Update send_sms.py

--- python_scripts/send_sms.py
+++ python_scripts/send_sms.py
@@ -4,16 +4,16 @@ from twilio.rest import Client
 
 def send_message(x):
     # These need to be changed to enviromental variables
-    account_sid = 'ACfcb6b435b76307064f38cf284157dc5d'
-    auth_token = 'ab5f2559a1821edfd8246993cfee0786'
+    account_sid = ''
+    auth_token = ''
 
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
                       body = x,
                       #Phone number also need to be change to enviromental variables
-                      from_ = '+17082219671',
-                      to = '+12085972537'
+                      from_ = '',
+                      to = ''
                   )
     print(message.sid)
 m = input("What do you want to say?:")
